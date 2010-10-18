507b8ec184193aa92764125f758aa09cca230377
Hanno Schlichting <hanno@hannosch.eu>
2010-10-18 17:19:56 +0200

    Complete engine tests

--- experimental/atrefspeedup/tests/test_engine.py
+++ experimental/atrefspeedup/tests/test_engine.py
@@ -22,6 +22,25 @@ class TestGetReferences(ATRefSpeedupTestCase):
         result = self.rc.getReferences(doc1, 'relatesTo')
         self.assertEquals(result[0].getTargetObject(), doc2)
 
+    def test_many(self):
+        doc1 = self.portal.doc1
+        doc2 = self.portal.doc2
+        doc3 = self.portal.doc3
+        uids = [doc2.UID(), doc3.UID()]
+        doc1.setRelatedItems(uids)
+        result = [r.getTargetObject() for r in self.rc.getReferences(doc1)]
+        self.assertEquals(set(result), set([doc2, doc3]))
+
+    def test_bidi(self):
+        doc1 = self.portal.doc1
+        doc2 = self.portal.doc2
+        doc1.setRelatedItems([doc2.UID()])
+        doc2.setRelatedItems([doc1.UID()])
+        result = [r.getTargetObject() for r in self.rc.getReferences(doc1)]
+        self.assertEquals(result, [doc2])
+        result = [r.getTargetObject() for r in self.rc.getReferences(doc2)]
+        self.assertEquals(result, [doc1])
+
 
 class TestGetBackReferences(ATRefSpeedupTestCase):
 
@@ -41,6 +60,27 @@ class TestGetBackReferences(ATRefSpeedupTestCase):
         self.assertEquals(result[0].getSourceObject(), doc1)
         result = self.rc.getBackReferences(doc2, 'relatesTo')
         self.assertEquals(result[0].getSourceObject(), doc1)
+
+    def test_many(self):
+        doc1 = self.portal.doc1
+        doc2 = self.portal.doc2
+        doc3 = self.portal.doc3
+        uids = [doc2.UID(), doc3.UID()]
+        doc1.setRelatedItems(uids)
+        result = [r.getSourceObject() for r in self.rc.getBackReferences(doc2)]
+        self.assertEquals(set(result), set([doc1]))
+        result = [r.getSourceObject() for r in self.rc.getBackReferences(doc3)]
+        self.assertEquals(set(result), set([doc1]))
+
+    def test_bidi(self):
+        doc1 = self.portal.doc1
+        doc2 = self.portal.doc2
+        doc1.setRelatedItems([doc2.UID()])
+        doc2.setRelatedItems([doc1.UID()])
+        result = [r.getSourceObject() for r in self.rc.getBackReferences(doc1)]
+        self.assertEquals(result, [doc2])
+        result = [r.getSourceObject() for r in self.rc.getBackReferences(doc2)]
+        self.assertEquals(result, [doc1])
 
 
 def test_suite():
