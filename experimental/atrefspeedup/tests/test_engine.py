ea19799d56b9c24dc8ab41e1a2d6c261701365b8
Hanno Schlichting <hanno@hannosch.eu>
2010-10-18 17:14:49 +0200

    Added tests for single case

--- experimental/atrefspeedup/tests/test_engine.py
+++ experimental/atrefspeedup/tests/test_engine.py
@@ -13,6 +13,15 @@ class TestGetReferences(ATRefSpeedupTestCase):
         self.assertEquals(self.rc.getReferences(doc1), [])
         self.assertEquals(self.rc.getReferences(doc1, 'relatesTo'), [])
 
+    def test_single(self):
+        doc1 = self.portal.doc1
+        doc2 = self.portal.doc2
+        doc1.setRelatedItems([doc2.UID()])
+        result = self.rc.getReferences(doc1)
+        self.assertEquals(result[0].getTargetObject(), doc2)
+        result = self.rc.getReferences(doc1, 'relatesTo')
+        self.assertEquals(result[0].getTargetObject(), doc2)
+
 
 class TestGetBackReferences(ATRefSpeedupTestCase):
 
@@ -20,10 +29,18 @@ class TestGetBackReferences(ATRefSpeedupTestCase):
         self.rc = getToolByName(self.portal, 'reference_catalog')
 
     def test_none(self):
-        # getBackReferences(self, object, relationship=None, targetObject=None):
         doc1 = self.portal.doc1
         self.assertEquals(self.rc.getBackReferences(doc1), [])
         self.assertEquals(self.rc.getBackReferences(doc1, 'relatesTo'), [])
+
+    def test_single(self):
+        doc1 = self.portal.doc1
+        doc2 = self.portal.doc2
+        doc1.setRelatedItems([doc2.UID()])
+        result = self.rc.getBackReferences(doc2)
+        self.assertEquals(result[0].getSourceObject(), doc1)
+        result = self.rc.getBackReferences(doc2, 'relatesTo')
+        self.assertEquals(result[0].getSourceObject(), doc1)
 
 
 def test_suite():
